963e112a1bdd7e44da4e4e045ff00b8016224fda
leazy <101962294+leazy12@users.noreply.github.com>
2023-11-14 00:06:30 +0800

    Add files via upload

---
+++ Latihan2/src/main/java/com/mycompany/latihan2/Latihan2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihan2;
+
+/**
+ *
+ * @author mkhai
+ */
+public class Latihan2 {
+
+    public static void main(String[] args) {
+        System.out.println("Muhammad Ridwan");
+        System.out.println("5B Reg Pagi BJB");
+        System.out.println("2110010233");
+    }
+}
